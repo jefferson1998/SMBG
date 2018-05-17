5cde04729af492f1f6d06673ac86ea89c33bb073
Rodrigo <Rodrigo@Rodrigo-PC>
2018-05-17 15:38:27 -0300

    tirei os comentarios

--- src/java/ModelSMBG/Persistivel.java
+++ src/java/ModelSMBG/Persistivel.java
@@ -1,26 +1,29 @@
-//package ModelSMBG;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity
-//public abstract class Persistivel {
-//    
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    protected long id;
-//     
-//    @Override
-//    public boolean equals(Object obj) {
-//        if(this == obj)
-//            return true;
-//        
-//        if(!(obj instanceof Persistivel))
-//            return false;
-//        
-//        Persistivel persistivel = (Persistivel) obj;
-//        return this.id == persistivel.id;
-//    }
-//}
+package ModelSMBG;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Persistivel implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    protected long id;
+     
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        
+        if(!(obj instanceof Persistivel))
+            return false;
+        
+        Persistivel persistivel = (Persistivel) obj;
+        return this.id == persistivel.id;
+    }
+}
