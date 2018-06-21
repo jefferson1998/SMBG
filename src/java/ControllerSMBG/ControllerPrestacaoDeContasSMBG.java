689ed53eab2a7ceaaafdc134237df530b78f455d
Rodrigo <Rodrigo@Rodrigo-PC>
2018-06-21 15:29:25 -0300

    fiz a prestacao de contas que tu falou mas não consegui testar que ta com frescura aki em casa

--- src/java/ControllerSMBG/ControllerPrestacaoDeContasSMBG.java
+++ src/java/ControllerSMBG/ControllerPrestacaoDeContasSMBG.java
@@ -1,12 +1,43 @@
 package controllerSMBG;
 
+import ModelSMBG.Funcionario;
+import ModelSMBG.PrestacaoDeConta;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
 
 @ManagedBean
 @SessionScoped
 public class ControllerPrestacaoDeContasSMBG {
 
-	
+    private PrestacaoDeConta prestacaoDeConta;
+    private Funcionario funcionarioQuePrestouConta;
+
+    public ControllerPrestacaoDeContasSMBG() {
+        prestacaoDeConta = new PrestacaoDeConta();
+        prestacaoDeConta.setValorTotalDoCombustivel(
+                prestacaoDeConta.getValorDoLitroDeCombustivel() * prestacaoDeConta.getQuantidadeDeLitrosAbastecido());
+        
+        prestacaoDeConta.setValorArrecadadoDescontado(
+                prestacaoDeConta.getValorTotalArrecadadoNoDia() - prestacaoDeConta.getValorTotalDoCombustivel() - 
+                        prestacaoDeConta.getSalarioDoMotorista() - prestacaoDeConta.getSalarioDoCobrador());
+        
+        funcionarioQuePrestouConta = null; //Pegar o nome do funcionario logado na sessao
+    }
+
+    public PrestacaoDeConta getPrestacaoDeConta() {
+        return prestacaoDeConta;
+    }
+
+    public void setPrestacaoDeConta(PrestacaoDeConta prestacaoDeConta) {
+        this.prestacaoDeConta = prestacaoDeConta;
+    }
+
+    public Funcionario getFuncionarioQuePrestouConta() {
+        return funcionarioQuePrestouConta;
+    }
+
+    public void setFuncionarioQuePrestouConta(Funcionario funcionarioQuePrestouConta) {
+        this.funcionarioQuePrestouConta = funcionarioQuePrestouConta;
+    }
+    
 }
